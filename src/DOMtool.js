f51751ed6e55423be0e4a1a57ca631724ceec232
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-08-08 02:19:38 -0400

    -feat: add svg functions

--- src/DOMtool.js
+++ src/DOMtool.js
@@ -16,4 +16,46 @@ function addListeners(element, events){
   }
 }
 
-export { createElement, addListeners };
+function createSVG(viewBox, path){
+  const svgns = "http://www.w3.org/2000/svg";
+
+  const svgElement = document.createElementNS(svgns, "svg");
+  svgElement.setAttribute("viewBox", viewBox);
+  const svgPath = document.createElementNS(svgns, "path");
+  svgPath.setAttribute("d", path);
+
+  svgElement.appendChild(svgPath);
+
+  svgElement.classList.add("icon");
+
+  return svgElement;
+}
+
+function applySvgGradient(svgElement, colors , degree = '0', id = 'gradient'
+){
+  const svgns = "http://www.w3.org/2000/svg";
+
+  const defs = document.createElementNS(svgns, 'defs');
+  const linearGradient = document.createElementNS(svgns, 'linearGradient');
+
+  linearGradient.setAttribute("x1", "0%");
+  linearGradient.setAttribute("y1", "0%");
+  linearGradient.setAttribute("x2", "100%");
+  linearGradient.setAttribute("y2", "100%");
+  linearGradient.setAttribute("id", id);
+  linearGradient.setAttribute('gradientTransform', `rotate(${degree})`);
+
+  colors.forEach( (color, index) => {
+    const stop = document.createElementNS(svgns, 'stop');
+    stop.setAttribute('offset', `${(index/(colors.length-1)) * 100}%`);
+    stop.setAttribute('stop-color', `${color}`);
+    linearGradient.appendChild(stop);
+  })
+  defs.appendChild(linearGradient);
+  svgElement.appendChild(defs);
+
+  const targetElement = svgElement.querySelector('path, rect, circle, ellipse, polygon, polyline');
+  targetElement.setAttribute("fill", `url(#${id})`);
+}
+
+export { createElement, addListeners, createSVG, applySvgGradient};
