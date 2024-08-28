8f2da6d9cf301ecee6dec4c15ba29484faceb8e1
Carson Bates <carson.d.bates.27@dartmouth.edu>
2024-08-27 20:01:56 -0400

    -feat: separate TaskManager and Task classes and add storage and edit methods to TaskManager

---
+++ src/data/taskManager.js
@@ -0,0 +1,78 @@
+import {Task} from '../model/task.js';
+
+export class TaskManager {
+  static taskLibrary = TaskManager.loadFromStorage();
+  static idCounter = JSON.parse(localStorage.getItem('storage-task-id-counter')) || 1;
+
+  static getTask(taskId){
+    let matchingTask;
+    TaskManager.getAllTasks().forEach(task => {
+     if(task.id == taskId){
+      matchingTask = task;
+      }
+    });
+    return matchingTask;
+  }
+
+  static getAllTasks(){
+    return TaskManager.taskLibrary;
+  }
+
+  static addTask(details, id){
+    TaskManager.taskLibrary.push({
+      details: details,
+      id: id});
+    TaskManager.saveToStorage();
+  }
+
+  static deleteTask(taskId){
+    let newTaskLibrary = [];
+
+    TaskManager.taskLibrary.forEach(task => {
+      if(task.id != taskId){
+        newTaskLibrary.push(task);
+      }
+    });
+
+    TaskManager.taskLibrary = newTaskLibrary;
+    TaskManager.saveToStorage();
+  }
+
+  static editTask(id, title, description, workTime, dueDate, progress, tag){
+    let task = TaskManager.getTask(id).details;
+    console.log(task);
+    task.setTitle(title);
+    task.setDescription(description);
+    task.setWorkTime(workTime);
+    task.setDueDate(dueDate);
+    task.setProgress(progress);
+    task.setTag(tag);
+    TaskManager.saveToStorage();
+  }
+
+  static idCounterIncrement(){
+    TaskManager.idCounter++; 
+    TaskManager.saveToStorage();
+  }
+
+  static getIdCounter(){
+    return TaskManager.idCounter;
+  }
+
+  static saveToStorage(){
+    localStorage.setItem('storage-tasks', JSON.stringify(TaskManager.taskLibrary))
+    localStorage.setItem('storage-task-id-counter', JSON.stringify(TaskManager.idCounter));
+  }
+
+  static loadFromStorage(){
+    const storedInfo = JSON.parse(localStorage.getItem('storage-tasks')) || [];
+    console.log(storedInfo);
+    const processedInfo = storedInfo.map(task => ({
+      details: (new Task(task.details.title, task.details.description, task.details.dueDate, task.details.workTime, task.details.progress, task.details.tag)),
+      id: task.id
+    }));
+    console.log(processedInfo);
+    return processedInfo; 
+
+  }
+}
---
+++ src/model/task.js
@@ -0,0 +1,60 @@
+import { createElement, addListeners, createSVG, applySvgGradient, createIconButton} from '../tool/DOMtool.js';
+
+export class Task {
+  constructor(title, description, dueDate, workTime, progress, tag){
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.workTime = workTime;
+    this.progress = progress;
+    this.tag = tag;
+  }
+
+  getTitle(){
+    return this.title;
+  }
+
+  setTitle(title){
+    this.title = title;
+  }
+
+  getDescription(){
+    return this.description;
+  }
+
+  setDescription(description){
+    this.description = description;
+  }
+
+  getDueDate() {
+    return this.dueDate;
+  }
+
+  setDueDate(dueDate){
+    this.dueDate = dueDate;
+  }
+
+  getWorkTime(){
+    return this.workTime;
+  }
+
+  setWorkTime(workTime){
+    this.workTime = workTime;
+  }
+
+  getProgress(){
+    return this.progress;
+  }
+
+  setProgress(progress){
+    this.progress = progress;
+  }
+
+  getTag(){
+    return this.tag;
+  }
+  
+  setTag(tag){
+    this.tag = tag;
+  }
+}
